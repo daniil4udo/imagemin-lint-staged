d4400ae0862c0d91762480218ee2f47100299233
daniil4udo <daniil4udo@gmail.com>
2022-11-23 11:35:45 +0400

    feature: add _silentErrors | better logging

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -10,36 +10,36 @@ import { minifyFile } from '../lib/index.mjs'
 const exec = util.promisify(child_process.exec)
 
 describe('index module', () => {
-	const FILENAMES = [
-		'./__fixtures__/test.gif',
-		'./__fixtures__/test.jpg',
-		'./__fixtures__/test.png',
-		'./__fixtures__/test.svg',
-	]
+  const FILENAMES = [
+    './__fixtures__/test.gif',
+    './__fixtures__/test.jpg',
+    './__fixtures__/test.png',
+    './__fixtures__/test.svg',
+  ]
 
-	const stats = () =>
-		Promise.all(
-			FILENAMES.map(async (f) => {
-				const { size } = await stat(path.resolve(__dirname, f))
-				return { f, size }
-			}),
-		)
+  const stats = () =>
+    Promise.all(
+      FILENAMES.map(async (f) => {
+        const { size } = await stat(path.resolve(__dirname, f))
+        return { f, size }
+      }),
+    )
 
-	describe('minifyFile function', () => {
-		it('should work as expected', async () => {
-			const before = await stats()
+  describe('minifyFile function', () => {
+    it('should work as expected', async () => {
+      const before = await stats()
 
-			await Promise.all(
-				FILENAMES.map(f => minifyFile(path.resolve(__dirname, f))),
-			)
+      await Promise.all(
+        FILENAMES.map(f => minifyFile(path.resolve(__dirname, f))),
+      )
 
-			const after = await stats()
+      const after = await stats()
 
-			await exec('git checkout .')
-			expect(after).not.toEqual(before)
+      await exec('git checkout .')
+      expect(after).not.toEqual(before)
 
-			expect(before).toMatchSnapshot()
-			expect(after).toMatchSnapshot()
-		})
-	})
+      expect(before).toMatchSnapshot()
+      expect(after).toMatchSnapshot()
+    })
+  })
 })
