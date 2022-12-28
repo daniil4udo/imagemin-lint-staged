16ccc5f073f8eddd7b8a6aa70dcd79c8371ed3ed
daniil4udo <daniil4udo@gmail.com>
2022-12-28 14:12:50 +0400

    fix-type: move all types to env

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,15 @@
+/// <reference types="@types/node" />
+
 /// <reference types="@types/imagemin-gifsicle" />
 /// <reference types="@types/imagemin-mozjpeg" />
 /// <reference types="@types/imagemin-optipng" />
 /// <reference types="@types/imagemin-svgo" />
 /// <reference types="@types/imagemin-webp" />
-/// <reference types="@types/node" />
+
+/// <reference types="@democrance/utils" />
+/// <reference types="cosmiconfig" />
+/// <reference types="defu" />
+/// <reference types="pretty-bytes" />
+/// <reference types="unbuild" />
 
 export { }
